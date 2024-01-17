eed2e853079c836320028eff1aedc04c500f6fa6
Yoto Kim <yongtokim1215@gmail.com>
2024-01-16 17:50:55 -0800

    Added setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+__version__ = ""
+__maintainer__ = ""
+__email__ = ""
+
+setup(
+    name='crapgpt',
+    version=__version__,
+    description=(
+        "Fine-tuned LLM to predict aspects of an individual's diet given their microbiome profile"
+    )
+
+)
