d160362ad8b94023275e987c48583a4006b05b8d
Arjun Kohli <arjun.kohli0707@gmail.com>
2024-01-25 17:36:58 -0800

    preliminary command line changes

---
+++ fine_tuning.py
@@ -0,0 +1,27 @@
+import click
+import crap_gpt._parameter_descriptions as desc
+
+# Allow using -h to show help information
+# https://click.palletsprojects.com/en/7.x/documentation/#help-parameter-customization
+CTXSETS = {"help_option_names": ["-h", "--help"]}
+
+@click.command()
+@click.option(
+    '--config-json',
+    required=True,
+    type=click.Path(exists=True),
+    help=desc.CONFIG_JSON
+)
+@click.option(
+    '-c', '--continue-training',
+    required=False, default=False, is_flag=True,
+    help=desc.CONTINUE_TRAINING
+)
+@click.option(
+    '--output-model-summary',
+    required=False, default=False, is_flag=True,
+    help=desc.OUTPUT_MODEL_SUMMARY
+)
+def transfer_learning(config_json, continue_training, output_model_summary):
+    pass
+
